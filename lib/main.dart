c7441aba1fa848c12fa86b33b31d965207d59ef3
Anatpc2022 <ana.tpc2022@gmail.com>
2026-04-06 21:59:12 -0300

    SnackBar e Desfazendo Ação

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,8 @@ class _HomeState extends State<Home> {
   final _todoController = TextEditingController();
 
   List _toDoList = [];
+  Map<String, dynamic>? _lastRemoved;
+  int? _lastRemovedPos;
 
   @override
   void initState() {
@@ -103,27 +105,62 @@ class _HomeState extends State<Home> {
             child: ListView.builder(
               padding: EdgeInsets.only(top: 10),
               itemCount: _toDoList.length,
-              itemBuilder: (context, index) {
-                return CheckboxListTile(
-                  title: Text(_toDoList[index]["title"]),
-                  value: _toDoList[index]["OK"],
-                  onChanged: (value) {
-                    setState(() {
-                      _toDoList[index]["OK"] = value;
-                      _saveData();
-                    });
-                  },
-                  secondary: CircleAvatar(
-                    child: Icon(
-                      _toDoList[index]["OK"] ? Icons.check : Icons.error,
-                    ),
-                  ),
-                );
-              },
+              itemBuilder: builderItem,
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget builderItem(BuildContext context, int index) {
+    return Dismissible(
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["OK"],
+        onChanged: (value) {
+          setState(() {
+            _toDoList[index]["OK"] = value;
+            _saveData();
+          });
+        },
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["OK"] ? Icons.check : Icons.error),
+        ),
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved!["title"]}\" removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos!, _lastRemoved);
+                  _saveData();
+                });
+              },
+            ),
+            duration: Duration(seconds: 2),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
